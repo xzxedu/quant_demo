26ca9ea11b8d4b26ce3f1204e6b563170eff48d4
xzxedu <xzxtju@outlook.com>
2019-10-13 22:18:44 +0800

    change production setting

--- config/production_setting.py
+++ config/production_setting.py
@@ -1,1 +1,14 @@
 # 生产环境的配置
+# -*- coding: utf-8 -*-
+
+DEBUG = True
+SQLALCHEMY_ECHO = True
+SQLALCHEMY_DATABASE_URI = 'mysql://root:wechatdemo123@127.0.0.1/quant_db?charset=utf8mb4'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+SQLALCHEMY_ENCODING = "utf8mb4"
+
+APP = {
+    'domain':'https://xzx.faithforfuture.com'
+}
+
+RELEASE_VERSION="20190929001"
